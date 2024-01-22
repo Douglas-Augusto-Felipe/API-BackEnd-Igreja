d80c1e8ec9c3c18880a7abc9354fc98e0f45414e
Douglas Augusto <douglas.full.ti@gmail.com>
2024-01-22 20:04:43 -0300

    add git spring

---
+++ src/main/java/Igreja/Mocidade/MocidadeApplication.java
@@ -0,0 +1,13 @@
+package Igreja.Mocidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MocidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MocidadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Igreja/Mocidade/MocidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package Igreja.Mocidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MocidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
